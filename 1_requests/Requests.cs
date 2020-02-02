dd666ca32a9416c7b8cf54ed160e74ebf299e365
patevs <ppevans11@gmail.com>
2020-02-02 17:08:54 +1300

    ✨  Requests.cs

--- 1_requests/Requests.cs
+++ 1_requests/Requests.cs
@@ -6,28 +6,27 @@ using System;
 using System.IO;
 using System.Net;
 // using System.Net.Http;
+// using System.Threading.Tasks; // .Task;
 
 namespace Requests
 {
     class Program
     {
       /*
-      public void Get() {
-        Console.WriteLine("Making GET Request...");
-        string html = string.Empty;
-        string url = @"https://api.stackexchange.com/2.2/answers?order=desc&sort=activity&site=stackoverflow";
+      public async Task<string> GetAsync(string uri)
+      {
+          HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+          request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
 
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-        request.AutomaticDecompression = DecompressionMethods.GZip;
-
-        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-        using (Stream stream = response.GetResponseStream())
-        using (StreamReader reader = new StreamReader(stream))
-        {
-            html = reader.ReadToEnd();
-        }
-
-        Console.WriteLine(html);
+          using(HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+          using(Stream stream = response.GetResponseStream())
+          using(StreamReader reader = new StreamReader(stream))
+          {
+            Task<string> result = await reader.ReadToEndAsync();
+            Console.WriteLine(result);
+            return result;
+            // return await reader.ReadToEndAsync();
+          }
       }
       */
 
@@ -54,6 +53,7 @@ namespace Requests
         // Get();
         string url = @"https://api.stackexchange.com/2.2/answers?order=desc&sort=activity&site=stackoverflow";
         Get(url);
+        // GetAsync(url);
       }
     }
 }
