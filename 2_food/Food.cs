d59ce1bca24940b62220dd32123ae63662a58207
patevs <ppevans11@gmail.com>
2020-02-02 17:18:32 +1300

    ✨  Food.cs

--- 2_food/Food.cs
+++ 2_food/Food.cs
@@ -27,8 +27,9 @@ namespace Food
       }
       */
 
-      public static void GetRequest() {
-        string uri = @"https://api.edamam.com/api/food-database/parser";
+      public static void GetRequest(string uri) {
+        // 'https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id={your app_id}&app_key={your app_key}'
+        // string uri = @"https://api.edamam.com/api/food-database/parser";
         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
         // request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
 
@@ -45,7 +46,12 @@ namespace Food
       static void Main(string[] args)
       {
         Console.WriteLine("\n --- FOOD DATABASE --- \n");
-        GetRequest();
+        // construct url
+        string app_id = "cdfcb58a";
+        string app_key = "567dc307d8be8fa2b06d58b0658a94cf";
+        string endpoint = "https://api.edamam.com/api/food-database/parser";
+        string uri = @endpoint + "?ingr=red%20apple&app_id=" + app_id + "&" + app_key;
+        GetRequest(uri);
       }
     }
 }
