267aa030fb6b15e36ac1e5459403e737261a480c
patevs <ppevans11@gmail.com>
2020-02-02 15:02:21 +1300

    ✨  run.ps1

--- 1_examples/run.ps1
+++ 1_examples/run.ps1
@@ -17,8 +17,13 @@ function CheckCommand($cmdname) {
 
 # --------------------------------------------------------------------------
 
-Write-Host "`n Compiling Examples.cs `n" -ForegroundColor Green
-# Write-Host " Detecting Python Installation... " -NoNewLine
+Write-Host @'
+
+  Compiling Examples.cs
+  ---------------------
+'@
+
+Write-Host "  * Detecting C# Compiler Installation... " # -NoNewLine
 
 # Verify C# compiler is installed and available
 
@@ -26,15 +31,30 @@ $csc = "C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe"
 
 if (CheckCommand $csc){
     # https://stackoverflow.com/questions/6338015/how-do-you-execute-an-arbitrary-native-command-from-a-string
-    # $command = 'python -V'
-    $cscExists = Invoke-Expression $csc # Can use iex instead of Invoke-Expression
-    Write-Host $cscExists -ForegroundColor Green -NoNewLine
+    $command = "$csc /help | grep 'Compiler version'"
+    $cscVersion = Invoke-Expression $command # Can use iex instead of Invoke-Expression
+    Write-Host "  * C# Compiler: " -NoNewLine
+    Write-Host $cscVersion -ForegroundColor Green -NoNewLine
 } else {
-    Write-Host "`n`n Csc Installation Could Not Be Found! Exiting..." -ForegroundColor Red
+    Write-Host "`n`n C# Compiler Installation Could Not Be Found! Exiting..." -ForegroundColor Red
     exit
 }
 
-# C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe
-#           /t:exe /out:MyApplication.exe MyApplication.cs
+# Compile Example.cs
+
+function compile {
+  $command = "$csc /t:exe /out:Examples.exe Examples.cs"
+  Invoke-Expression $command
+}
+
+# Execute the program
+
+function execute {
+  $command = "./Examples"
+  Invoke-Expression $command
+}
+
+compile
+execute
 
 # EOF #
